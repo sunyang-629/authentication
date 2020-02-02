a1a99094a1a5f0f7c5a67e7b3e3592af50405560
sunyang9876 <sunyang9876@hotmail.com>
2020-02-02 21:34:09 +1000

    add authentication

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const bodyParser = require('body-parser');
-const localStrategy = require('passport-local');
+const LocalStrategy = require('passport-local');
 const passportLocalMongoose = require('passport-local-mongoose');
 const User = require('./models/user');
 mongoose.connect("mongodb://localhost:27017/TWDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require('express-session')({
     secret: 'Rusty is the best and cutest dog in the world',
     resave: false,
@@ -17,16 +18,59 @@ app.use(require('express-session')({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
+//===============================================================
 
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/secret', (req, res) => {
+app.get('/secret', isLoggedIn, (req, res) => {
     res.render('secret');
 })
+
+app.get('/register', (req, res) => {
+    res.render('register');
+})
+
+app.post('/register', (req, res) => {
+    User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect('/secret');
+            })
+        }
+    })
+})
+
+app.get("/login", (req, res) => {
+    res.render('login');
+})
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: '/secret',
+    failureRedirect: 'login'
+}), (req, res) => {  
+})
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
 
 app.listen(3567, () => {
     console.log('server started......');
